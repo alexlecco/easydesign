76b4a9b9c42bbd4a7c2c48c7e03414636e8e0155
alexlecco <alexlecco24@gmail.com>
2016-05-06 16:05:08 -0300

    [validating username format]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
 
   devise :omniauthable, omniauth_providers: [:facebook, :twitter]
 
+  validates :username, presence: true, uniqueness: true,
+                       legth: { in: 5..20,
+                                too_short: "El usuario debe tener al menos 5 caracteres",
+                                too_long: "El usuario no debe superar los 20 caracteres" }
+                       format: { with: /([A-Za-z0-9\-\_]+)/, message: "El usuario puede contener solo letras, numeros y guiones" }
+
+  #validación personalizada
+  #validate: :validacion_personalizada, on: :create
+
   def self.find_or_create_by_omniauth(auth)
     user = User.where(provider: auth[:provider], uid: auth[:uid]).first
 
@@ -21,4 +30,15 @@ class User < ActiveRecord::Base
       )
     end
   end
+
+=begin
+  private
+  def validacion_personalizada
+    if true
+
+    else
+      errors.add(:username, "Tu usuario no es valido")
+    end
+  end
+=end
 end
