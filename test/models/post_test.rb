08dd366ae9e4f44fafc7c5e3fe684e63920e9369
alexlecco <alexlecco24@gmail.com>
2016-05-17 18:40:40 -0300

    [custom tests added]

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,26 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "se debe poder crear un post" do
+    post = Post.create(title: "Mi titulo", content: "Mi contenido")
+    assert post.save
+  end
+
+  test "se debe poder actualizar un post" do
+    post = posts(:primer_articulo)
+    assert post.update(title: "Nuevo titulo", content: "Nuevo contenido")
+  end
+
+  test "se debe poder encontrar un post por su id" do
+    post_id = posts(:primer_articulo).id
+    assert_nothing_raised { Post.find(post_id) }
+  end
+
+  test "se debe poder borrar un post" do
+    post = posts(:primer_articulo)
+    post.destroy
+    assert_raise(ActiveRecord::RecordNotFound) {Post.find(post.id)}
+  end
+
 end
